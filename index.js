0c48fe18a91f5afbfa22c0ef98d5756fb097ab86
Omar <otantawy53@gmail.com>
2022-04-29 09:51:13 -0400

    commit tree 39de82a92e7dabb04340f2da281c93f125f4d1e7

--- index.js
+++ index.js
@@ -1,3 +1,60 @@
 const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
 
 // Code your solution here
+
+// let totalBatteries = () => {
+//     return batteryBatches.reduce((accumulator, item) => {
+//         let total = item.price;
+//     })
+// }
+
+//^^^^#1 not addding stuff*******************************************************************************
+
+// let totalBatteries = (accumulator , item) => {
+//     let total = accumulator + item;
+//     return total;
+// }
+
+// batteryBatches.reduce(totalBatteries, 0);
+
+//^^^^#2 not adding stuff**********************************************************************************
+
+// let totalBatteries = adder.reduce((accumulator, item) => {
+//     return accumulator + item;
+// } ,0)
+
+//^^^^#3 saying totalBatteries variable doesnt exist*******************************************************
+
+// let totalBatteries = () => {
+//     return batteryBatches.reduce((accumulator, item) => {
+//         let total = accumulator + item;
+//         console.log(total);
+//         return total;
+//     } , 0)
+// }
+
+
+//^^^^#4 still not returning anything, might need to use map***************************************************
+
+let totalBatteries = batteryBatches.reduce((accumulator, item) => accumulator + item , 0);
+
+// console.log(totalBatteries);
+
+//^^^^#5 CORRECT ANSWER, GOT IT FROM MDN********************************************************************
+
+
+// const array1 = [1, 2, 3, 4];
+
+// const sumWithInitial = array1.reduce((previousValue, currentValue) => previousValue + currentValue, 0);
+
+// console.log(sumWithInitial);
+
+//^^^^ example problem from mdn*********************************************************************************
+
+// let totalBatteries = batteryBatches.reduce((accumulator, item) => accumulator + item , 0);
+
+// console.log(totalBatteries);
+
+
+//^^^^#6 ********************************************************************
+
